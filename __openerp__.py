70d1554f0f90f0a99734c9f5d4eb8868055c4378
Luis B <unbroken@live.com.mx>
2018-04-26 22:12:57 -0500

    subit todo

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+{
+    "name": "MRP Default Location for Raw Materials and Finished Products",
+    "version": "1.6",
+    "author": "Argil Consulting",
+    "category": "",
+    "website": "",
+    "license": "",
+    "depends": [
+        "mrp",
+        "product"
+    ],
+    "demo": [],
+    "data": [
+        "product_category_view.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
